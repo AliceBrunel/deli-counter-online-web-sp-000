63c3afa5165ea48882ce38db37062541722f1a06
Alice Brunel <alc.brunel@gmail.com>
2019-07-07 15:40:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,5 @@
 # Write your code here.
+katz_deli = []
 
 def line
   #show the current place in the line
@@ -7,3 +8,9 @@ def line
 end
 
 def take_a_number(array,name)
+  #puts the person's name along with their position in the line
+end
+
+def now_serving
+  #puts the next person in line and then remove them from the front.
+  #If there is nobody in line, puts ""
